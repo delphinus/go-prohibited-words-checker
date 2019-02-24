e829aaa0e7365ca4a49ecea1cb83575c8683116a
delphinus <delphinus@remora.cx>
2019-02-24 21:07:10 +0900

    Separate walker from action

--- action.go
+++ action.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
-	"os"
 	"path/filepath"
-	"regexp"
-	"strings"
 
-	ignore "github.com/sabhiram/go-gitignore"
 	"golang.org/x/xerrors"
 	"gopkg.in/urfave/cli.v2"
 )
@@ -24,91 +18,6 @@ func Action(cli *cli.Context) error {
 		return xerrors.Errorf(": %w", err)
 	}
 	Log(cli, w.ResultHeader())
-	return nil
-}
-
-// Walker is a struct for walking dir
-type Walker struct {
-	found     []string
-	gitignore *ignore.GitIgnore
-	ignore    *regexp.Regexp
-	scanned   int
-	skipped   int
-	words     [][]byte
-}
-
-// NewWalker is a constructor of Walker
-func NewWalker() (w *Walker, err error) {
-	matcher, err := Config.GitIgnore()
-	if err != nil {
-		return nil, xerrors.Errorf(": %w", err)
-	}
-	w = &Walker{
-		gitignore: matcher,
-		words:     make([][]byte, len(Config.Words)),
-	}
-	for i, word := range Config.Words {
-		w.words[i] = []byte(word)
-	}
-	ignores := strings.Join(Config.Ignores, "|")
-	if w.ignore, err = regexp.Compile(ignores); err != nil {
-		return nil, xerrors.Errorf(": %w", err)
-	}
-	return
-}
-
-func (w *Walker) walk(path string, info os.FileInfo, err error) error {
-	if err != nil {
-		return xerrors.Errorf(": %w", err)
-	}
-	rel, err := filepath.Rel(Config.Dir, path)
-	if err != nil {
-		return xerrors.Errorf(": %w", err)
-	}
-	if info.IsDir() {
-		return w.walkDir(rel)
-	}
-	return w.walkFile(rel, path)
-}
-
-func (w *Walker) walkDir(rel string) error {
-	// sabhiram/go-gitignore cannot ignore directories without trailing
-	// slash for the .gitignore entries with tariling slashes. such as...
-	//
-	//   in .gitignore:
-	//     /node_modules/
-	//   the directory:
-	//     node_modules
-	if !strings.HasSuffix(rel, "/") {
-		rel += "/"
-	}
-	if w.gitignore.MatchesPath(rel) {
-		return filepath.SkipDir
-	}
-	return nil
-}
-
-func (w *Walker) walkFile(rel string, path string) error {
-	if w.gitignore.MatchesPath(rel) || w.ignore.MatchString(rel) {
-		w.scanned++
-		w.skipped++
-		return nil
-	}
-	content, err := ioutil.ReadFile(path)
-	if err != nil {
-		return xerrors.Errorf("reading from %s: %w", rel, err)
-	}
-	contained := false
-	for _, word := range w.words {
-		if bytes.Contains(content, word) {
-			contained = true
-			break
-		}
-	}
-	if contained {
-		w.found = append(w.found, path)
-	}
-	w.scanned++
 	return nil
 }
 
